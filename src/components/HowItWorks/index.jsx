0aff5c03a8bf33d3b321aa95a43b37001b4327ee
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-03-11 18:06:15 +0000

    Styles changes

--- src/components/HowItWorks/index.jsx
+++ src/components/HowItWorks/index.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 
-const HowItWorks = () => {
-    return (
-        <main className="how-it-works">
-            <section className="steps">
-                <h1>How It Works</h1>
-                <ol>
-                    <li>
-                        <strong>Search:</strong> Enter your desired location or
-                        browse through the list of available classrooms on
-                        campus.
-                    </li>
-                    <li>
-                        <strong>View Availability:</strong> Get real-time
-                        updates on classroom occupancy. See which rooms are
-                        available or reserved for classes.
-                    </li>
-                    <li>
-                        <strong>Enjoy Studying:</strong> Head over to your
-                        chosen classroom and get to work. It's that easy!
-                    </li>
-                </ol>
-            </section>
-        </main>
-    );
-};
+const HowItWorks = () => (
+    <main className="how-it-works">
+        <section className="steps">
+            <h1>How It Works</h1>
+            <ol>
+                <strong>Search:</strong>
+                <p>
+                    Enter your desired location or browse through the list of
+                    available classrooms on campus.
+                </p>
+
+                <strong>View Availability:</strong>
+                <p>
+                    Get real-time updates on classroom occupancy. See which
+                    rooms are available or reserved for classes.
+                </p>
+
+                <strong>Enjoy Studying:</strong>
+                <p>
+                    Head over to your chosen classroom and get to work. It's
+                    that easy!
+                </p>
+            </ol>
+        </section>
+    </main>
+);
 
 export default HowItWorks;
--- src/components/MeetTeam/index.jsx
+++ src/components/MeetTeam/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MeetTeam = () => (
     <main class="meet-the-team">
         <section class="team-intro">
-            <h1>Welcome to Ashesi Classroom Finder</h1>
+            <h1>Meet the team</h1>
             <p>
                 Our team, made up of fellow Ashesi University students,
                 understands the challenges of finding the perfect study spot,
